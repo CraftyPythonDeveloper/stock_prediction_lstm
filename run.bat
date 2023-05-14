786c8ccba6b3c2770531cf8975df64adf2702e2e
Amit Yadav <amityadav4664@gmail.com>
2023-05-14 19:51:09 +0530

    Changed anaconda path in run.bat. Also added context manager for sql connection

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off    
-set PATH=C:\ProgramData\Anaconda3\Scripts
+set PATH=C:\Users\%USERNAME%\Anaconda3\Scripts
 call %PATH%\activate.bat
 cd stock_prediction
 python stock_prediction_nse.py
+cd ..
